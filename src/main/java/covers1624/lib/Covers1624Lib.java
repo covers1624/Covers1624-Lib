ccf4ccf01dc643e452cf0dab96074f1fcca2cd36
covers1624 <laughlan.cov@internode.on.net>
2016-01-28 22:36:20 +1030

    Small update to main class, corrects some loading issues.

--- src/main/java/covers1624/lib/Covers1624Lib.java
+++ src/main/java/covers1624/lib/Covers1624Lib.java
@@ -3,12 +3,10 @@ package covers1624.lib;
 import com.google.common.eventbus.EventBus;
 import com.google.common.eventbus.Subscribe;
 import covers1624.lib.handler.ConfigurationHandler;
+import covers1624.lib.reference.Reference;
 import covers1624.lib.util.ItemUtils;
 import covers1624.lib.util.LogHelper;
-import cpw.mods.fml.common.DummyModContainer;
-import cpw.mods.fml.common.LoadController;
-import cpw.mods.fml.common.MetadataCollection;
-import cpw.mods.fml.common.ModMetadata;
+import cpw.mods.fml.common.*;
 import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 import cpw.mods.fml.common.versioning.VersionParser;
@@ -17,66 +15,25 @@ import cpw.mods.fml.relauncher.IFMLLoadingPlugin;
 
 import java.util.Map;
 
-@IFMLLoadingPlugin.MCVersion("1.7.10")
-public class Covers1624Lib extends DummyModContainer implements IFMLLoadingPlugin {
+@Mod(modid = Reference.MOD_ID, name = Reference.MOD_NAME, version = Reference.MOD_VERSION, useMetadata = false, acceptedMinecraftVersions = "[1.7.10]")
+public class Covers1624Lib{
 
-	public static ModMetadata modMetadata = new ModMetadata();
-
-	static {
-		modMetadata.modId = "Covers1624Core";
-		modMetadata.name = "Covers1624 Core";
-		modMetadata.description = "Contains Basic and some advanced Utility classes used by all my mods.";
-		modMetadata.version = "1.7.10-1.0";
-		modMetadata.authorList.add("covers1624");
-	}
-
-	public Covers1624Lib() {
-		super(modMetadata);
-	}
-
-
-	@Override
-	public String[] getASMTransformerClass() {
-		return new String[0];
-	}
-
-	@Override
-	public String getModContainerClass() {
-		return this.getClass().getName();
-	}
-
-	@Override
-	public String getSetupClass() {
-		return null;
-	}
-
-	@Override
-	public void injectData(Map<String, Object> data) {
-
-	}
-
-	@Override
-	public String getAccessTransformerClass() {
-		return null;
-	}
-
-	@Override
-	public boolean registerBus(EventBus bus, LoadController controller) {
-		bus.register(this);
-		return true;
-	}
-
-	@Subscribe
+	@Mod.EventHandler
 	public void preInit(FMLPreInitializationEvent event) {
+		event.getModMetadata();
 		ConfigurationHandler.init(event.getSuggestedConfigurationFile());
 	}
 
-	@Subscribe
+	@Mod.EventHandler
 	public void postInit(FMLPostInitializationEvent event) {
 	}
 
-	@Override
-	public VersionRange acceptableMinecraftVersionRange() {
-		return VersionParser.parseRange("[1.7.10]");
+	//TODO push this to 1.8.9 branch.
+	private static void parseModMetadata(ModMetadata metadata){
+		metadata.modId = Reference.MOD_ID;
+		metadata.name = Reference.MOD_NAME;
+		metadata.description = "Contains Basic and some advanced Utility classes used by all my mods.";
+		metadata.version = Reference.MOD_VERSION;
+		metadata.authorList.add("covers1624");
 	}
 }
